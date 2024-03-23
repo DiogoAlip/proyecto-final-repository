88e608ed197bec48cf2b69bd7438c96c8f4f1c6c
Sebaco106 <sebariv2011@hotmail.com>
2024-03-22 20:42:12 -0500

    Cambie todo y uso py con template con jinja2 con flask

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Lógica para el mapa interactivo
+// Por ahora, solo un ejemplo básico de funcionalidad de clic
+document.getElementById('mapa').addEventListener('click', function(event) {
+    alert('Has hecho clic en el mapa');
+});
